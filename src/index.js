0a9be092ad9838777a4e89ed9b2820e70d92aec8
Frontend JavaScript <frontendjavascript@iMAC-07.local>
2025-03-13 20:49:26 +0200

    mycv

--- src/index.js
+++ src/index.js
@@ -7,4 +7,9 @@ import './scss/styles.scss'
 // Import https://github.com/mattboldt/typed.js/
 // import Typed from 'typed.js';
 import '../node_modules/typed.js/dist/typed.umd.js'
+
+import particles from 'particles.js'
+
+import'./js/particles.js'
+
 import './js/main.js';
