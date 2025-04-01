7193a5736a2bdb505234277abf1d3f31b445629d
Shiniky1 <mihai_cazan99@yahoo.com>
2025-04-01 13:53:26 +0300

    mycv

--- src/js/main.js
+++ src/js/main.js
@@ -227,6 +227,21 @@ import GLightbox from 'glightbox';
         });
     }
 
+     // Validare
+     const forms = document.querySelectorAll('.requires-validation')
+     Array.from(forms)
+         .forEach(function (form) {
+             form.addEventListener('submit', function (event) {
+                 if (!form.checkValidity()) {
+                     event.preventDefault()
+                     event.stopPropagation()
+                 }
+ 
+                 form.classList.add('was-validated')
+             }, false)
+         });
+ 
+
     /**
      * Initiate Pure Counter 
      */
